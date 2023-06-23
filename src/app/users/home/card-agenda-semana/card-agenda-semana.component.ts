480fe2e20bfd287305972b15ffd65357fcc5cf52
Flavia <flavia.banchieri@infortechms.com.br>
2023-06-23 17:11:00 -0400

    Ajustes

--- src/app/users/home/card-agenda-semana/card-agenda-semana.component.ts
+++ src/app/users/home/card-agenda-semana/card-agenda-semana.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgIf, NgFor} from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+
+];
+
+/**
+ * @title Binding event handlers and properties to the table rows.
+ */
 @Component({
   selector: 'app-card-agenda-semana',
   templateUrl: './card-agenda-semana.component.html',
-  styleUrls: ['./card-agenda-semana.component.scss']
+  styleUrls: ['./card-agenda-semana.component.scss'],
+  standalone: true,
+  imports: [MatTableModule, NgIf, NgFor],
 })
-export class CardAgendaSemanaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CardAgendaSemanaComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  clickedRows = new Set<PeriodicElement>();
 }
--- src/app/users/home/card-agenda-semana/dist/card-agenda-semana.component.js
+++ src/app/users/home/card-agenda-semana/dist/card-agenda-semana.component.js
@@ -8,16 +8,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 exports.CardAgendaSemanaComponent = void 0;
 var core_1 = require("@angular/core");
+var common_1 = require("@angular/common");
+var table_1 = require("@angular/material/table");
+var ELEMENT_DATA = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+];
+/**
+ * @title Binding event handlers and properties to the table rows.
+ */
 var CardAgendaSemanaComponent = /** @class */ (function () {
     function CardAgendaSemanaComponent() {
+        this.displayedColumns = ['position', 'name', 'weight', 'symbol'];
+        this.dataSource = ELEMENT_DATA;
+        this.clickedRows = new Set();
     }
-    CardAgendaSemanaComponent.prototype.ngOnInit = function () {
-    };
     CardAgendaSemanaComponent = __decorate([
         core_1.Component({
             selector: 'app-card-agenda-semana',
             templateUrl: './card-agenda-semana.component.html',
-            styleUrls: ['./card-agenda-semana.component.scss']
+            styleUrls: ['./card-agenda-semana.component.scss'],
+            standalone: true,
+            imports: [table_1.MatTableModule, common_1.NgIf, common_1.NgFor]
         })
     ], CardAgendaSemanaComponent);
     return CardAgendaSemanaComponent;

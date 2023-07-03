3f3040cf927327f738f6ce7f34c91be3103c2501
Flavia <flavia.banchieri@infortechms.com.br>
2023-07-03 14:49:22 -0400

    ajustes anotacao

--- src/app/users/home/card-anotacao/card-anotacao.component.ts
+++ src/app/users/home/card-anotacao/card-anotacao.component.ts
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardAnotacaoComponent implements OnInit {
 
+
+  anotacoes = [
+    { id: 1, check: true, arquivado: false, anotacao: 'Comprar água' },
+    { id: 2, check: false, arquivado: false, anotacao: 'Agendar consulta com paciente X' },
+    { id: 3, check: false, arquivado: false, anotacao: 'Preparar material para a próxima sessão' },
+    { id: 4, check: true, arquivado: false, anotacao: 'Enviar lembrete de consulta para paciente Y' },
+    { id: 5, check: false, arquivado: false, anotacao: 'Revisar anotações da última sessão' },
+    { id: 6, check: true, arquivado: false, anotacao: 'Participar de reunião com a equipe' },
+    { id: 7, check: false, arquivado: false, anotacao: 'Ligar para fornecedor de material de escritório' },
+    { id: 8, check: true, arquivado: false, anotacao: 'Organizar agenda da semana seguinte' },
+    { id: 9, check: false, arquivado: false, anotacao: 'Enviar relatório de atendimentos para a diretoria' },
+    { id: 10, check: true, arquivado: false, anotacao: 'Receber pagamento do paciente Z' },
+    { id: 11, check: true, arquivado: false, anotacao: 'Atualizar prontuário do paciente X' }
+  ];
+
+
+
   constructor() { }
+
+  checkAnotacao(){
+
+  }
 
   ngOnInit(): void {
   }
--- src/app/users/home/card-anotacao/dist/card-anotacao.component.js
+++ src/app/users/home/card-anotacao/dist/card-anotacao.component.js
@@ -10,7 +10,22 @@ exports.CardAnotacaoComponent = void 0;
 var core_1 = require("@angular/core");
 var CardAnotacaoComponent = /** @class */ (function () {
     function CardAnotacaoComponent() {
+        this.anotacoes = [
+            { id: 1, check: true, arquivado: false, anotacao: 'Comprar água' },
+            { id: 2, check: false, arquivado: false, anotacao: 'Agendar consulta com paciente X' },
+            { id: 3, check: false, arquivado: false, anotacao: 'Preparar material para a próxima sessão' },
+            { id: 4, check: true, arquivado: false, anotacao: 'Enviar lembrete de consulta para paciente Y' },
+            { id: 5, check: false, arquivado: false, anotacao: 'Revisar anotações da última sessão' },
+            { id: 6, check: true, arquivado: false, anotacao: 'Participar de reunião com a equipe' },
+            { id: 7, check: false, arquivado: false, anotacao: 'Ligar para fornecedor de material de escritório' },
+            { id: 8, check: true, arquivado: false, anotacao: 'Organizar agenda da semana seguinte' },
+            { id: 9, check: false, arquivado: false, anotacao: 'Enviar relatório de atendimentos para a diretoria' },
+            { id: 10, check: true, arquivado: false, anotacao: 'Receber pagamento do paciente Z' },
+            { id: 11, check: true, arquivado: false, anotacao: 'Atualizar prontuário do paciente X' }
+        ];
     }
+    CardAnotacaoComponent.prototype.checkAnotacao = function () {
+    };
     CardAnotacaoComponent.prototype.ngOnInit = function () {
     };
     CardAnotacaoComponent = __decorate([

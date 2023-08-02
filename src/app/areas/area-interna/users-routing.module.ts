f00948c0220797cbf876e75e17a38652fb96dabe
Flavia Banchieri <flavia.banchieri@infortechms.com.br>
2023-08-01 21:22:47 -0400

    Ajuste layout

--- src/app/areas/area-interna/users-routing.module.ts
+++ src/app/areas/area-interna/users-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home";
+import { LayoutComponent } from "./layout/layout.component";
 
 const routes: Routes = [
     {
-        path: '', component: HomeComponent,
+        path: '', component: LayoutComponent,
         children: [
             { path: '', component: HomeComponent }
         ]
